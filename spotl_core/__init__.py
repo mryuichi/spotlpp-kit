5e1d9d133ea7834d314d245518d5546016accdd6
Ryuichi Matsukura <ryuichi@sfc.wide.ad.jp>
2025-09-20 22:01:17 +0900

    Create __init__.py

---
+++ spotl_core/__init__.py
@@ -0,0 +1,3 @@
+from .roles import SpotlPP
+from .normalizers_en import norm_number, norm_percent, norm_money, norm_date, month_to_num
+from .scoring_pp import role_alignment_pp, verdict_from_scores_pp, aggregate_sentence_pp
